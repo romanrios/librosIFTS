14d59bfd25a036e1c861a98608b9eec374fc1e2e
romanrios <romanrios@live.com>
2025-05-24 12:15:20 -0300

    update 24-5

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const links = document.querySelectorAll("a");
 links.forEach((link) => link.setAttribute("target", "_blank"));
+
+const loginBtn = document.getElementById("login-btn");
+if (loginBtn) {
+  loginBtn.addEventListener("click", () => {
+    window.open(
+      "https://aulasvirtuales.bue.edu.ar/login/index.php",
+      "_blank",
+      "noopener,noreferrer"
+    );
+  });
+}
